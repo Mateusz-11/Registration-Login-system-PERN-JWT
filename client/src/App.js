47fd5bba2dba13cf90d2be1b21d7b814a6c39862
Mateusz-11 <tweak.night@gmail.com>
2023-07-03 23:05:22 +0200

    Set up React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,62 @@
+import React, { useState } from "react";
 import "./App.css";
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Routes,
+	Navigate,
+} from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	const setAuth = (boolean) => {
+		setIsAuthenticated(boolean);
+	};
+
 	return (
 		<>
-			<h1>test 2023</h1>
+			<Router>
+				<div className='container'>
+					<Routes>
+						<Route
+							path='/login'
+							element={
+								!isAuthenticated ? (
+									<Login setAuth={setAuth} />
+								) : (
+									<Navigate to='/dashboard' />
+								)
+							}
+						/>
+						<Route
+							path='/register'
+							element={
+								!isAuthenticated ? (
+									<Register setAuth={setAuth} />
+								) : (
+									<Navigate to='/login' />
+								)
+							}
+						/>
+						<Route
+							path='/dashboard'
+							element={
+								isAuthenticated ? (
+									<Dashboard setAuth={setAuth} />
+								) : (
+									<Navigate to='/login' />
+								)
+							}
+						/>
+					</Routes>
+				</div>
+			</Router>
 		</>
 	);
 }
